409372b5a2e251f6781f4c65dae673bb7d5c08dd
marloboyer <marlo.boyer7@yahoo.com>
2023-03-11 11:25:19 -0600

    created views and partcials in the views to add the new relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,22 @@ class Product < ApplicationRecord
   validates :description, length: { in: 10..500 }
 
   belongs_to :supplier
+
+  def is_discounted?
+    if price <= 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    product_tax = 0.09 * price
+    return product_tax
+  end
+
+  def total
+    product_total = price + tax
+    return product_total
+  end
 end
---
+++ app/views/products/_product.json.jbuilder
@@ -0,0 +1,10 @@
+json.id product.id
+json.name product.name
+json.price product.price
+json.tax product.tax
+json.total product.total
+json.description product.description
+json.inventory product.inventory
+json.supplier product.supplier
+json.is_discounted? product.is_discounted?
+json.images product.images
