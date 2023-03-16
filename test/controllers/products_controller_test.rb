28f3cacd0174b9bfe220958cf5f5f0e6953ea692
marloboyer <marlo.boyer7@yahoo.com>
2023-03-15 19:48:27 -0500

    Added authorization to orders index action, adds admin attribute to users table and adds authenticate_admin method to application controller, and updated products conroller test for create, update and deystroy

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  #index test
+  setup do
+    # creating an admin user
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+
+    # creating a session
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+
+    # saving jwt to instance variable
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -10,46 +21,69 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal Product.count, data.length
   end
 
-  #show test
   test "show" do
     get "/products/#{Product.first.id}.json"
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "tax", "total", "description", "inventory", "supplier", "is_discounted?", "images"], data.keys
-    assert_equal true, data.keys.include?("name")
+    assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "description", "inventory", "supplier", "images"], data.keys
   end
 
-  #create test
   test "create" do
+    # testing a successful product create
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, description: "test description" }
+      post "/products.json",
+           headers: { "Authorization" => "Bearer #{@jwt}" },
+           params: { supplier_id: Supplier.first.id, name: "Lego Millennium Falcon", price: 800, description: "Defend the Galaxy and build the largest LEGO Star Wars Millennium Falcon to date! The perfect set for adult Star Wars fans and expert builders, This starship will inspire hours of play recreating the films or can be displayed as a collectible toy model" }
       assert_response 200
     end
-    post "/products.json", params { }
+
+    # testing validation failures
+    post "/products.json",
+         headers: { "Authorization" => "Bearer #{@jwt}" },
+         params: {}
     assert_response 422
+
+    # testing unauthorize user
+    post "/products.json"
+    assert_response 401
   end
 
-  #update test
   test "update" do
-    product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Update name" }
+    #  testing successful update action
+    product = Product.second
+    patch "/products/#{product.id}.json",
+          headers: { "Authorization" => "Bearer #{@jwt}" },
+          params: { name: "Updated name" }
+
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal "Update name", data["name"]
+    assert_equal "Updated name", data["name"]
     assert_equal product.price, data["price"].to_i
     assert_equal product.description, data["description"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    # testing failed validations
+    patch "/products/#{product.id}.json",
+          headers: { "Authorization" => "Bearer #{@jwt}" },
+          params: { name: "" }
     assert_response 422
+
+    # testing unauthorized user
+    patch "/products/#{product.id}.json"
+    assert_response 401
   end
 
-  #destroy test
   test "destroy" do
+    # testing successful destroy action
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json",
+             headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
+
+    # testing unauthorized user
+    delete "/products/#{Product.first.id}.json"
+    assert_response 401
   end
 end
