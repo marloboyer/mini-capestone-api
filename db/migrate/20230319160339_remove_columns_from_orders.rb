20405e193d49a14a9e6e0846a3e0caa371d02462
marloboyer <marlo.boyer7@yahoo.com>
2023-03-19 11:21:14 -0500

    Created a Carted Products model with the attributes of user id, product id, quantity, order id and status, removed product id and quantity from orders model. Created model associations for product, orders, user, and carted products.

---
+++ db/migrate/20230319160339_remove_columns_from_orders.rb
@@ -0,0 +1,7 @@
+class RemoveColumnsFromOrders < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :orders, :product_id, :integer
+    remove_column :orders, :quantity, :integer
+
+  end
+end
